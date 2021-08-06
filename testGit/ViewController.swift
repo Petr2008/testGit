ffba3d0afe3341864f9ee59c8a64d51df24935ca
Petr Gusakov <pit2008apple@gmail.com>
2021-08-06 18:23:41 +0300

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Petr Gusakov on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
